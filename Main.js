978b5676c55897212c7c82c69c0f58c9a48cae19
Patrick <tricksap@gmail.com>
2023-01-28 16:52:01 +0800

    automatic download
    download the certificate and put it on the folder named auto certificate

--- Main.js
+++ Main.js
@@ -1,26 +1,67 @@
 require("dotenv").config();
+const fs = require("fs");
+const axios = require("axios");
 const cloudinary = require("cloudinary").v2;
+const homeDir = require("os").homedir();
+
+const targetDir = `${homeDir}/Desktop/AutoCertificate/`;
+// const desktopDir = `${homeDir}/Desktop/AutoCertificate/`;
+
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir);
+  console.log("folder Created");
+}
+
+let names = ["fofo babar", "babarr fooba"];
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const url = cloudinary.url("1664802569356wp6100717_arypi7", {
-  transformation: [
-    {
-      overlay: {
-        font_family: "Poppins",
-        font_size: 100,
-        font_weight: "bold",
-        text: "Patrick Ray P Sapusao sapusao sapusao",
-      },
-      color: "#333",
-      effect: "colorize",
-      y: "0",
-      width: "1000",
-    },
-  ],
-});
+//download image
+const download_image = (url, image_path) =>
+  axios({
+    url,
+    responseType: "stream",
+  }).then(
+    (response) =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on("finish", () => resolve())
+          .on("error", (e) => reject(e));
+      })
+  );
 
-console.log(url);
+for (n of names) {
+  //customize the certificate with the specific names in the var n
+  const url = cloudinary.url("5819472-01_hxyono", {
+    transformation: [
+      {
+        overlay: {
+          font_family: "Montserrat",
+          font_size: 100,
+          font_weight: "bold",
+          text: n,
+        },
+        color: "#14b1ab",
+        effect: "colorize",
+        y: "-50",
+        width: "1500",
+      },
+    ],
+  });
+
+  ////cloudinary upload code
+  // cloudinary.uploader
+  //   .upload(url)
+  //   .then((result) => console.log(result.secure_url));
+
+  //call download function
+  (async () => {
+    await download_image(url, targetDir + n + ".png");
+
+    console.log(n + "Done"); // true
+  })();
+}
